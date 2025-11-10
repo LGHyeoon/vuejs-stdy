77d3ef235db15c70e5cfcac9f12d45951cfdf30d
LGHyeoon <a01043355935@gmail.com>
2025-11-10 21:56:31 +0900

    import CommonHeader file

---
+++ src/components/common/CommonHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>
+            <a href="#" class="logo">
+                <img alt="Home Logo" src="../../assets/home_logo.png" style="width: 250px; height: 100px;" />
+            </a>
+        </h1>
+    </header>
+</template>
+
+<script>
+
+export default {
+    
+}
+
+</script>
