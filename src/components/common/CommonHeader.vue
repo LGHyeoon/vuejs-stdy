42c2e6f0839bdd5b32a99b47c4946102f03f8b67
LGHyeoon <a01043355935@gmail.com>
2025-11-10 22:01:23 +0900

    add menu list

--- src/components/common/CommonHeader.vue
+++ src/components/common/CommonHeader.vue
@@ -2,8 +2,12 @@
     <header>
         <h1>
             <a href="#" class="logo">
-                <img alt="Home Logo" src="../../assets/home_logo.png" style="width: 250px; height: 100px;" />
+                <img alt="Home Logo" src="../../assets/home_logo.png" style="width: 280px; height: 80px;" />
             </a>
+            <a href="#" class="myHf">My HF</a>
+            <a href="#" class="enroll">신청</a>
+            <a href="#" class="addSmplDoc">간편 서류제출</a>
+            <a href="#" class="custService">고객서비스</a>
         </h1>
     </header>
 </template>
@@ -15,3 +19,7 @@ export default {
 }
 
 </script>
+
+<style>
+
+</style>
